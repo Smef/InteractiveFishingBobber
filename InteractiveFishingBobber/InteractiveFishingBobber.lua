c3c99c0a3d675ac24130b4d8236504870ea9caa5
Smef <david@bluefeathergroup.com>
2022-11-15 00:44:48 -0500

    Added fishing bobber files

---
+++ InteractiveFishingBobber/InteractiveFishingBobber.lua
@@ -0,0 +1,36 @@
+local frame = CreateFrame("FRAME", "InteractiveBobberFrame");
+frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_START");
+frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
+
+local originalArc = GetCVar("SoftTargetInteractArc");
+local originalRange = GetCVar("SoftTargetInteractRange");
+
+
+local function eventHandler(self, event, unitTarget, castGUID, spellID)
+
+	-- print("Event - " .. event .. " - " .. spellID);
+	 if (event == "UNIT_SPELLCAST_CHANNEL_START" and spellID == 131476) then
+		-- print("Start - " .. event .. " - " .. spellID);
+		originalArc = GetCVar("SoftTargetInteractArc");
+		originalRange = GetCVar("SoftTargetInteractRange");
+
+		-- print("Original Arc: " .. originalArc);
+		-- print("Original Range: " .. originalArc);
+
+		SetCVar( "SoftTargetInteractArc", 2 );
+		SetCVar( "SoftTargetInteractRange", 30 );
+		-- print("Updated Arc: " .. 2);
+		-- print("Updated Range: " .. 30);
+
+	end
+
+	if (event == "UNIT_SPELLCAST_CHANNEL_STOP" and spellID == 131476) then
+		-- print("Stop - " .. event .. " - " .. spellID);
+		-- print("Resetting Arc to: " .. originalArc);
+		-- print("Resetting Arc to: " .. originalRange);
+
+		SetCVar( "SoftTargetInteractArc", originalArc );
+		SetCVar( "SoftTargetInteractRange", originalRange );
+	end
+end
+frame:SetScript("OnEvent", eventHandler);
