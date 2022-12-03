d626440e0ae84917cfcb90935ec07a33342883b8
Dillan Prince <dillanprince@gmail.com>
2022-12-02 19:48:16 -0600

    feat: During the fishing channel, set SFX Volume to 1, the maximum. After the channel ends, reset SFX Volume to its original value.

--- InteractiveFishingBobber/InteractiveFishingBobber.lua
+++ InteractiveFishingBobber/InteractiveFishingBobber.lua
@@ -4,38 +4,45 @@ frame:RegisterEvent("UNIT_SPELLCAST_CHANNEL_STOP");
 
 local originalArc = GetCVar("SoftTargetInteractArc");
 local originalRange = GetCVar("SoftTargetInteractRange");
-
+local originalSfxVolume = GetCVar("Sound_SFXVolume");
 
 local function eventHandler(self, event, unitTarget, castGUID, spellID)
 	if spellID ~= 131476 then return end
 	if unitTarget ~= "player" then return end
 
-
-	-- print ("Target - " .. unitTarget)
-
+	-- print("Target - " .. unitTarget)
 	-- print("Event - " .. event .. " - " .. spellID);
-	 if (event == "UNIT_SPELLCAST_CHANNEL_START") then
+
+	if (event == "UNIT_SPELLCAST_CHANNEL_START") then
 		-- print("Start - " .. event .. " - " .. spellID);
+
 		originalArc = GetCVar("SoftTargetInteractArc");
 		originalRange = GetCVar("SoftTargetInteractRange");
+		originalSfxVolume = GetCVar("Sound_SFXVolume");
 
 		-- print("Original Arc: " .. originalArc);
-		-- print("Original Range: " .. originalArc);
+		-- print("Original Range: " .. originalRange);
+		-- print("Original SFX Volume: " .. originalSfxVolume);
 
-		SetCVar( "SoftTargetInteractArc", 2 );
-		SetCVar( "SoftTargetInteractRange", 30 );
+		SetCVar("SoftTargetInteractArc", 2);
+		SetCVar("SoftTargetInteractRange", 30);
+		SetCVar("Sound_SFXVolume", 1);
+
 		-- print("Updated Arc: " .. 2);
 		-- print("Updated Range: " .. 30);
-
+		-- print("Updated SFX Volume: " .. 1);
 	end
 
 	if (event == "UNIT_SPELLCAST_CHANNEL_STOP") then
 		-- print("Stop - " .. event .. " - " .. spellID);
 		-- print("Resetting Arc to: " .. originalArc);
-		-- print("Resetting Arc to: " .. originalRange);
+		-- print("Resetting Range to: " .. originalRange);
+		-- print("Resetting SFX Volume to: " .. originalSfxVolume);
 
-		SetCVar( "SoftTargetInteractArc", originalArc );
-		SetCVar( "SoftTargetInteractRange", originalRange );
+		SetCVar("SoftTargetInteractArc", originalArc);
+		SetCVar("SoftTargetInteractRange", originalRange);
+		SetCVar("Sound_SFXVolume", originalSfxVolume);
 	end
 end
+
 frame:SetScript("OnEvent", eventHandler);
